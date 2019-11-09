b8e4e0f1820a85d96b71df2ba4320605c87f24f9
kami-sa <kamisa1999@yandex.ru>
2019-11-09 15:50:21 +0300

    Initial commit

---
+++ 25.1.py
@@ -0,0 +1,30 @@
+import random, string
+
+error = ('I', 'l', 'o', 'O', '1', '0')
+
+
+def generate_password(m):
+    password = ""
+    count = 0
+    while count != m:
+        flag = random.choice([True, False])
+        if flag:
+            symbol = random.choice(string.ascii_letters)
+        else:
+            symbol = random.choice(string.digits)
+        if symbol not in error:
+            password += symbol
+            count += 1
+    return password
+
+
+def main(n, m):  # n - колтчество m - длина
+    passwords = []
+    for i in range(n):
+        passwords.append(generate_password(m))
+    return passwords
+
+
+print('Случайный пароль из 7 символов:', generate_password(7))
+print('10 случайных паролей длиной 15 символов:')
+print(*main(10, 15), sep='\n')
